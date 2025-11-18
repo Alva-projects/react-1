10f4654031b0feedffe347fce8d6986e170dd508
Alva Lundberg <alva.lundberg@hotmail.se>
2025-11-18 09:02:48 +0100

    Added Github
    Will add hyperlink

--- react-assignment-1/src/components/ProfileCard.jsx
+++ react-assignment-1/src/components/ProfileCard.jsx
@@ -10,8 +10,8 @@ function ProfileCard () {
         <h2 className="Title">Portfolio</h2>
         <div className="ProfileCard">
             <Profile screenshot={screenshotBhromaon} name={"Bhromaon"} repo={"https://github.com/Alva-projects/bhromaon.git"} techUsed= {"This project was created with a time limit using HTML and CSS5."} writeUp= {"The project initially did not go as i planned it too. I tried to create all of the home page rather than only having time to create one part leaving the othe parts blank. It was a good challange wich made me practice prioritizing."}/>
-            <Profile screenshot={screenshotRecepies} name={"Recepies"} repo={"Lorem"} techUsed= {"Describe"} writeUp= {"Lorem ipsum"}/>
-            <Profile screenshot={screenshotDJ} name={"DJ"} repo={"Lorem"} techUsed= {"Describe"} writeUp= {"Lorem ipsum"}/>
+            <Profile screenshot={screenshotRecepies} name={"Recepies"} repo={"https://github.com/Alva-projects/assignment-7.version1.0.git"} techUsed= {"Describe"} writeUp= {"Lorem ipsum"}/>
+            <Profile screenshot={screenshotDJ} name={"DJ"} repo={"https://github.com/MPLarsson/Group-Project.git"} techUsed= {"Describe"} writeUp= {"Lorem ipsum"}/>
         </div>
         </>
     )
